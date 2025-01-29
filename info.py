b2d7fe2ec0a8986d46f076f6e6ea356cc5d9deb4
darkmk20 <jonidark2000@gmail.com>
2025-01-29 00:15:44 -0800

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "7615542336:AAHj4SFN91x-QWbwW9PFNAQK6FCB3qk
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://vault.pictures/p/9715d7d831754e54a47b0ea07179840d.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/d71a829bd664bbb21fc0a.jpg')).split()
 
 
 # Admins & Users
